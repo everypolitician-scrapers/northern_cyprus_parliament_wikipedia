8a0096c59167bb63e6394c53783e9445d15e0f05
Tony Bowden <tony@tmtm.com>
2015-09-13 20:10:00 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,40 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.xpath('//table[.//tr[contains(.,"Member of Parliament")]]').each do |table|
+    area = table.xpath('preceding-sibling::h2[1]/span[@class="mw-headline"]').text
+    table.xpath('.//tr[td]').each do |tr|
+      tds = tr.css('td')
+      data = { 
+        name: tds[0].text,
+        wikiname: tds[0].xpath('.//a[not(@class="new")]/@title').text,
+        party: tds[1].text.tidy,
+        area: area,
+        term: 14,
+        source: url,
+      }
+      ScraperWiki.save_sqlite([:name, :party, :area, :term], data)
+    end
+  end
+end
+
+scrape_list('https://en.wikipedia.org/wiki/14th_Parliament_of_Northern_Cyprus')
