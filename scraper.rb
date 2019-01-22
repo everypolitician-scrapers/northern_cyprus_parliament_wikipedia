43c70e750cc03d68a79b69f239f822911d9860b0
Tony Bowden <tony@tmtm.com>
2019-01-22 15:49:17 +0000

    add debugging output

--- scraper.rb
+++ scraper.rb
@@ -27,7 +27,7 @@ def scrape_list(url)
         term:     14,
         source:   url,
       }
-      # puts data
+      puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
       ScraperWiki.save_sqlite(%i(name party area term), data)
     end
   end
